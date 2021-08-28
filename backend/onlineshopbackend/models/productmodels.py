738e7fe1f75d9ac1508fdd92564bf44ad4b6219c
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-08-28 22:10:43 +0430

    price field added

--- backend/onlineshopbackend/models/productmodels.py
+++ backend/onlineshopbackend/models/productmodels.py
@@ -5,6 +5,7 @@ class ProductsModels(models.Model):
     product_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_created=True)
+    price = models.IntegerField()
 
     def __str__(self) -> str:
         return self.title
