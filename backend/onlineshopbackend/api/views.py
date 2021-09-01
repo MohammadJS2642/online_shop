247bdded5bdc1653d745e7a916bc6bdb04994306
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-09-01 19:02:25 +0430

    userList for create and list all users and UserDetail for retrieve and update users

--- backend/onlineshopbackend/api/views.py
+++ backend/onlineshopbackend/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from api.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class UserList(generics.ListCreateAPIView):
+    '''
+    get all users and create new user
+    '''
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateAPIView):
+    '''
+    for retrieve and update users
+    '''
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
